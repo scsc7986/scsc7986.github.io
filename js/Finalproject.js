c3654d090ffbe0fec150cf5a7bce65e0aa197cb4
Alfred Schweikert IV <143743691+scsc7986@users.noreply.github.com>
2023-12-17 23:07:17 -0700

    ok
    push

--- js/Finalproject.js
+++ js/Finalproject.js
@@ -1,31 +1,27 @@
 const inputField = document.getElementById("numberInput");
 const values = document.getElementById("values");
+const submitButton = document.getElementById("submitButton");
+const timerDisplay = document.getElementById("timer");
+
+let timer;
+let countdown = 15;
+
+// Start the timer when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  startTimer();
+});
 
 inputField.addEventListener("input", updateValue);
 
-const phoneNumberArray = [];
-
 function updateValue(e) {
-  const inputValue = e.target.value.toLowerCase();
-  const mappedNumber = mapNumber(inputValue);
-
-  if (mappedNumber === undefined) {
-    values.textContent = "Please enter a valid number";
-  } else {
-    phoneNumberArray.push(mappedNumber);
-
-    if (phoneNumberArray.length === 3 || phoneNumberArray.length === 7) {
-      phoneNumberArray.push("-");
-    }
-
-    values.textContent = phoneNumberArray.join("");
-    e.target.value = "";
-  }
+  const lowcase = e.target.value.toLowerCase();
+  values.textContent = mapNumbers(lowcase, e);
 }
 
-function mapNumber(word) {
+const phoneNumberArray = [];
+
+function mapNumbers(phoneNumber, e) {
   const numberMap = {
-    zero: "0",
     one: "1",
     two: "2",
     three: "3",
@@ -35,7 +31,48 @@ function mapNumber(word) {
     seven: "7",
     eight: "8",
     nine: "9",
+    zero: "0",
   };
 
-  return numberMap[word];
+  if (numberMap[phoneNumber] === undefined) {
+    return "Please enter a valid number";
+  }
+  phoneNumberArray.push(numberMap[phoneNumber]);
+
+  if (phoneNumberArray.length === 3 || phoneNumberArray.length === 7) {
+    phoneNumberArray.push("-");
+  }
+
+  if (phoneNumberArray.length === 12) {
+    // needs to be 12 because of the -'s
+    // block the input
+    inputField.disabled = true;
+    // Enable the submit button
+    submitButton.disabled = false;
+  }
+
+  e.target.value = "";
+  return phoneNumberArray.join("");
+}
+
+function submit() {
+  if (phoneNumberArray.length < 12) {
+    alert("Please enter a valid phone number");
+    return;
+  }
+  alert("You have entered a valid phone number");
+}
+
+function startTimer() {
+  // Set a timer for 15 seconds when the page loads
+  timer = setInterval(function () {
+    countdown--;
+    timerDisplay.textContent = `Timer: ${countdown}s`;
+
+    if (countdown <= 0) {
+      // Refresh the page after 15 seconds
+      clearInterval(timer);
+      location.reload();
+    }
+  }, 1000);
 }
